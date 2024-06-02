268645276c9186e8f8feccf48c1f43f89272b491
kgarcia <kgarcia@scl.edu.gt>
2024-06-02 14:32:01 -0600

    funcion

--- Calculadora.js
+++ Calculadora.js
@@ -1,14 +1,14 @@
-class Calculadora{
-    sumar (num1, num2){
+class Calculadora {
+    sumar(num1, num2) {
         return num1 + num2;
     }
-    resta (num1, num2){
+    restar(num1, num2) {
         return num1 - num2;
     }
-    dividir (num1, num2){
+    dividir(num1, num2) {
         return num1 / num2;
     }
-    multiplicar (num1, num2){
+    multiplicar(num1, num2) {
         return num1 * num2;
     }
 }
--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-let calculadora = document.querySelector(".container");
-
-calculadora.innerHTML = `
-    <div class="calculadora">
-        <div class="display">
-            <div id="valor-anterior"></div>
-            <div id="valor-actual"></div>
-        </div>
-        <button class="col-2">C</button>
-        <button>&larr;</button>
-        <button class="numero">%</button>
-        <button class="numero">7</button>
-        <button class="numero">8</button>
-        <button class="numero">9</button>
-        <button class="operador">X</button>
-        <button class="numero">4</button>
-        <button class="numero">5</button>
-        <button class="numero">6</button>
-        <button class="operador">-</button>
-        <button class="numero">1</button>
-        <button class="numero">2</button>
-        <button class="numero">3</button>
-        <button class="operador">+</button>
-        <button class="numero col-2">0</button>
-        <button class="numero">.</button>
-        <button class="operador">=</button>
+let container = document.querySelector(".container");
+    
+container.innerHTML = `
+<div class="calculadora">
+    <div class="display">
+        <div id="valor-anterior"></div>
+        <div id="valor-actual"></div>
     </div>
+    <button class="col-2" onclick="display.borrarTodo()"> C </button>
+    <button class="operador" onclick="display.borrar()">&larr;</button>
+    <button class="operador" value="dividir">/</button>   
+    <button class="numero">7</button>
+    <button class="numero">8</button>
+    <button class="numero">9</button>
+    <button class="operador" value="multiplicar">x</button>
+    <button class="numero">4</button>
+    <button class="numero">5</button>
+    <button class="numero">6</button>
+    <button class="operador" value="restar">-</button>
+    <button class="numero">1</button>
+    <button class="numero">2</button>
+    <button class="numero">3</button>
+    <button class="operador" value="sumar">+</button>
+    <button class="numero col-2">0</button>
+    <button class="numero">.</button>
+    <button class="operador" value="igual">=</button>
+</div>
 `;
